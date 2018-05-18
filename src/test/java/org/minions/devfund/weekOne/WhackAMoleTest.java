2d5c1f277cc7652fd9cc2b93e6eab9138ec3f5e5
Denis Parra Suzaño <triton.soporte@gmail.com>
2018-05-18 03:44:04 -0400

    fixed checkstyle to test file

--- src/test/java/org/minions/devfund/weekOne/WhackAMoleTest.java
+++ src/test/java/org/minions/devfund/weekOne/WhackAMoleTest.java
@@ -7,56 +7,84 @@ package org.minions.devfund.weekOne;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  *
  * @author Denis Parra
  */
 public class WhackAMoleTest {
-    
+
+    private static final int INIT_LENGTH = 10;
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+    private static final int THREE = 3;
+    private static final int FOUR = 4;
+    private static final int FIVE = 4;
+    private static final int NINE = 9;
     private WhackAMole whackAMole;
-    
+
+    /**
+     *  Setup test before each test.
+     */
     @Before
     public void setUp() {
-       whackAMole = new WhackAMole(10, 10);
+       whackAMole = new WhackAMole(INIT_LENGTH, INIT_LENGTH);
     }
 
+    /**
+     * Test to put a mole in a place.
+     */
     @Test
     public void testPlaceAMole() {
-        assertTrue(whackAMole.place(2, 2));
+        assertTrue(whackAMole.place(TWO, TWO));
     }
 
+    /**
+     *  Test try to put a mole in same place.
+     */
     @Test
     public void testPlaceAMoleInInvalidPlace() {
-        whackAMole.place(2, 2);
-        assertFalse(whackAMole.place(2, 2));
+        whackAMole.place(TWO, TWO);
+        assertFalse(whackAMole.place(TWO, TWO));
     }
 
+    /**
+     * Test to find a mole in a place.
+     */
     @Test
     public void testFoundAMole() {
-        whackAMole.place(2, 2);
-        whackAMole.whack(2, 2);
-        assertEquals(1, whackAMole.getScore());
-        assertEquals(9, whackAMole.getMolesLeft());
-        assertEquals(9, whackAMole.getAttemptsLeft());
+        whackAMole.place(TWO, TWO);
+        whackAMole.whack(TWO, TWO);
+        assertEquals(ONE, whackAMole.getScore());
+        assertEquals(NINE, whackAMole.getMolesLeft());
+        assertEquals(NINE, whackAMole.getAttemptsLeft());
     }
 
+    /**
+     *  Test to finad a module in a place when not exist.
+     */
     @Test
     public void testSearchAMoleWhenTheMoleIsNotInTheCoordinate() {
-        whackAMole.place(2, 2);
-        whackAMole.whack(2, 1);
+        whackAMole.place(TWO, TWO);
+        whackAMole.whack(TWO, ONE);
         assertEquals(0, whackAMole.getScore());
     }
 
+    /**
+     *  Test to print the grid to user.
+     */
     @Test
     public void testPrintUserGrid() {
-        whackAMole.place(0, 0);
-        whackAMole.place(1, 1);
-        whackAMole.place(2, 2);
-        whackAMole.place(3, 3);
-        whackAMole.place(4, 4);
-        whackAMole.place(5, 5);
+        whackAMole.place(ZERO, ZERO);
+        whackAMole.place(ONE, ONE);
+        whackAMole.place(TWO, TWO);
+        whackAMole.place(THREE, THREE);
+        whackAMole.place(FOUR, FOUR);
+        whackAMole.place(FIVE, FIVE);
         StringBuilder boardExpected = new StringBuilder();
         boardExpected.append("**********\n");
         boardExpected.append("**********\n");
@@ -71,14 +99,17 @@ public class WhackAMoleTest {
         assertEquals(boardExpected.toString(), whackAMole.printGridToUser());
     }
 
+    /**
+     *  Test to print the entire grid.
+     */
     @Test
     public void testPrintGrid() {
-        whackAMole.place(0, 0);
-        whackAMole.place(1, 1);
-        whackAMole.place(2, 2);
-        whackAMole.place(3, 3);
-        whackAMole.place(4, 4);
-        whackAMole.place(5, 5);
+        whackAMole.place(ZERO, ZERO);
+        whackAMole.place(ONE, ONE);
+        whackAMole.place(TWO, TWO);
+        whackAMole.place(THREE, THREE);
+        whackAMole.place(FOUR, FOUR);
+        whackAMole.place(FIVE, FIVE);
         StringBuilder boardExpected = new StringBuilder();
         boardExpected.append("M*********\n");
         boardExpected.append("*M********\n");
